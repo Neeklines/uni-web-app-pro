6562c73ff306a1211166e666ab5c8c31751a9ea0
Yehor <181513@stud.prz.edu.pl>
2026-03-26 22:44:43 +0100

    --amend

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -4,11 +4,11 @@ from app.models.user import User
 from app.database import get_db
 
 
-def get_current_user(x_user_id: int = Header(...)):
-    db: Session = Depends(get_db)
-
+def get_current_user(
+    x_user_id: int = Header(...),
+    db: Session = Depends(get_db),
+):
     user = db.query(User).filter(User.id == x_user_id).first()
-    db.close()
 
     if not user:
         raise HTTPException(status_code=401, detail="Invalid user")
